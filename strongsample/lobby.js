5fb4f61a0dea28ea0d25ad7affb0d5b219ec81d3
Stephen Shilale <sshilal1@binghamton.edu>
2017-05-01 13:16:53 -0400

    basic lobby functional

--- strongsample/lobby.js
+++ strongsample/lobby.js
@@ -2,17 +2,39 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var games = new Array();
+var Game = function(host,path) {
+    this.host = host;
+    this.path = path;
+};
+
 app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('A user connected');
+  console.log('A user: '+ socket.client.id +' connected');
 	
+	io.sockets.emit('gamesList', { games });
 	
+	socket.on('clientCreateGame', function(data) {
+		createNewGame(data);
+	});
 	
 });
 
+function createNewGame(gameinfo) {
+	var newNspPath = '/game' + gameinfo.id;
+	games.push(new Game(gameinfo.id,newNspPath));
+	io.sockets.emit('gamesList', { games });
+	
+	var nsp = io.of(newNspPath);
+	nsp.on('connection', function(game){
+		console.log('created a new game: ' + gameinfo.id);
+		nsp.emit('hi', 'Hello, and welcome to game ' + gameinfo.id);
+		io.sockets.emit('gamesList', { games });
+	});
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
